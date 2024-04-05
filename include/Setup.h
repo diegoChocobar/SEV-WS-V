f82b4f29afb971177592c34bd193cf90967d75d0
10GO_Mack <regitros.d@gmail.com>
2024-04-04 21:26:59 -0300

    Version 2.1

--- include/Setup.h
+++ include/Setup.h
@@ -81,7 +81,7 @@ void SaludoInicial(){
     lcd.setCursor(0, 0);
     lcd.print("***CDC Elctronics***");
     lcd.setCursor(0, 1);
-    lcd.print("*SEV-WS-V -> GIT-2.0");
+    lcd.print("*SEV-WS-V -> GIT-2.1");
     lcd.setCursor(0, 2);
     lcd.print("cel:+54-0387-5312963");
     lcd.setCursor(0, 3);
